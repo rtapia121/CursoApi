9ae8f8d04596d58e856b646630f504d09eb3d748
Ricardo Tapia <rtapia@121corp.com>
2021-08-09 14:06:54 -0500

    se genera modelo, controlador y todos archivos necesario para la creacion de categorias

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Category;
+use App\Http\Resources\CategoryResource;
+use App\Http\Requests\StoreCategoryRequest;
+use App\Http\Requests\UpdateCategoryRequest;
+
+class CategoryController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:sanctum')->except(['index', 'show']);
+    }
+
+    public function index()
+    {
+        return CategoryResource::collection(Category::paginate(5));
+    }
+
+    public function store(StoreCategoryRequest $request)
+    {
+        $category = Category::create($request->all());
+
+        return new CategoryResource($category);
+    }
+
+    public function show(Category $category)
+    {
+        return new CategoryResource($category);
+    }
+
+    public function update(UpdateCategoryRequest $request, Category $category)
+    {
+        $category->update($request->all());
+
+        return new CategoryResource($category);
+    }
+
+    public function destroy(Category $category)
+    {
+        $category->delete();
+
+        return response()->json();
+    }
+}
